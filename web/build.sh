fdda9572a5374c0580388d1c7065179282fae9b5
Tom <hortw1991@gmail.com>
2022-05-10 12:32:18 -0400

    Update build.sh

--- web/build.sh
+++ web/build.sh
@@ -1,6 +1,6 @@
 # Docker commands
-docker stop thorn91:personal
-docker rm $(docker ps -a -f status=exited -q)
-docker kill website
+sudo docker stop thorn91:personal
+sudo docker rm $(docker ps -a -f status=exited -q)
+sudo ocker kill website
 sudo docker build -t thorn91:personal .
 sudo docker run --name website --rm -d -p 80:80 thorn91:personal
